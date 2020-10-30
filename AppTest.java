340ef0991667ceb15b5812495f570b42be715531
BaoLe <71818128+BaoLe1998@users.noreply.github.com>
2020-10-30 17:04:05 +0700

    Delete AppTest.java

--- AppTest.java
+++
@@ -1,9 +0,0 @@
-package Exc81;
-
-public class AppTest {
-    public static void main(String[] args) {
-        EmployeeManagement employeeManagement = new EmployeeManagement();
-        employeeManagement.printAllEmployees();
-        System.out.print("Max Salary Employee: " + employeeManagement.getMaxSalaryEmployee());
-    }
-}
